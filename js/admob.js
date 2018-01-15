0349469dc5c56b9da1ee9261a7f873b2d3aaf235
kshravi86 <kshravi86@gmail.com>
2018-01-15 22:00:18 +0530

    heh

--- js/admob.js
+++ js/admob.js
@@ -1,8 +1,8 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-5830283354936773/3053337704',
-       interstitial: 'ca-app-pub-5830283354936773/9258162713'
+        banner: 'ca-app-pub-5830283354936773/6409472247',
+       interstitial: 'ca-app-pub-5830283354936773/709991499'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
